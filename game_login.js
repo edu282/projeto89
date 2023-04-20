0e8b856a850a580198e489d5e4458b7cd7f33816
edu282 <91227097+edu282@users.noreply.github.com>
2023-04-20 16:45:24 -0300

    Add files via upload

--- game_login.js
+++ game_login.js
@@ -1,10 +1,10 @@
-function addUser() /* Adicionar o nome addUser à function. */
-{
-    player1_name = document.getElementById("player1_name_input").value; /* Adicionar o id do jogador 1, ver no index.html */
-    player2_name = document.getElementById("player2_name_input").value; /* Adicionar o id do jogador 2, ver no index.html */
-  
-      localStorage.setItem("player1_name", player1_name); /* Coloque o nome da chave dentro das aspas */
-      localStorage.setItem("player2_name", player2_name); /* Coloque o nome da chave dentro das aspas */
-  
-      window.location = "game_page.html";
-  }
+
+function addUser() {
+  player1_name = document.getElementById("player1_name_input").value;
+  player2_name = document.getElementById("player2_name_input").value;
+
+    localStorage.setItem("player1_name", player1_name);
+    localStorage.setItem("player2_name", player2_name);
+
+    window.location = "game_page.html";
+}
